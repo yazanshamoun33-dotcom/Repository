66164e41daae05ba9ca3ce64cfe8675aebe8042f
yazanshamoun33-dotcom <yazanshamoun33@gmail.com>
2026-08-17 22:21:27 +0300

    Add portfolio certification file links

---
+++ assets/certifications/certifications-links.js
@@ -0,0 +1,9 @@
+// Certification source files used by the portfolio.
+// Generated from the certificates uploaded to assets/certifications.
+window.YAZAN_CERTIFICATIONS = [
+  { title: 'HVAC Systems Design', file: 'assets/certifications/HVAC%20COURSE.pdf' },
+  { title: 'Plumbing Systems Design', file: 'assets/certifications/Plumbing%20Course.pdf' },
+  { title: 'Fire Fighting Systems Design', file: 'assets/certifications/Fire%20Fighting%20Course.pdf' },
+  { title: 'Revit Mechanical', file: 'assets/certifications/ATC_EM30387809840300_Revit%20Mechanical%20(1).pdf' },
+  { title: 'BIM MEP Automation by Dynamo', file: 'assets/certifications/ATC_EM30434709867325_BIM%20MEP%20Automation%20by%20Dynamo.pdf' }
+];
